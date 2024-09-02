ed4e61e2dd65bb65f4b7b3b06d42103b2a594562
sundharesan11 <sundharesansk11@gmail.com>
2024-09-02 19:18:35 +0530

    sent-trans and cos_sim

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def load_css(file_path):
     with open(file_path, "r") as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-load_css("utilities/styles.css")
+load_css("git .css")
 
 def initialize_firebase():
     if not firebase_admin._apps:
